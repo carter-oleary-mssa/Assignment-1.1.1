98c2af94826aee22797989c6d6de6d221aeee0d9
carter-oleary-mssa <carteroleary.mssa@gmail.com>
2025-04-07 17:07:04 -0400

    Added files

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Enter a name: ");
+string userName = Console.ReadLine();
+Console.Write("Enter an age: ");
+int userAge = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter an address");
+string userAddress = Console.ReadLine();
+Console.WriteLine($"Name: {userName}\nAge: {userAge}\nAddress: {userAddress}");
